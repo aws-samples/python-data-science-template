fabb3b31fcd34816067bb7f9b977d14dee17403e
Verdi March <verdimrc@users.noreply.github.com>
2022-09-16 16:49:43 +0800

    Backport recent configs from a2rl (awslabs/amazon-accessible-rl-sdk)

---
+++ {{cookiecutter.repo_name}}/requirements/README.md
@@ -0,0 +1,7 @@
+| File                     | Description                                   |
+| ------------------------ | --------------------------------------------- |
+| `requirements.txt`       | Mandatory requirement                         |
+| `requirements-test.txt`  | Required for unit tests                       |
+| `requirements-docs.txt`  | Required to generate the html documentations. |
+| `requirements-build.txt` | Required to build the wheel.                  |
+| `requirements-dev.txt`   | Recommended packages during development.      |
--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -1,7 +1,7 @@
-raise ValueError(
-    "Baseline setup.py from cookiecutter aws-samples/python-data-science-template. "
-    "Please review and modify accordingly, then remove this exception"
-)
+# raise ValueError(
+#     "Baseline setup.py from cookiecutter aws-samples/python-data-science-template. "
+#     "Please review and modify accordingly, then remove this exception"
+# )
 
 import os
 from typing import List
@@ -13,21 +13,55 @@ _pkg: str = "{{cookiecutter.package_name}}"
 _version = "0.0.1"
 
 
-def read(fname) -> str:
+def read_lines(fname: str) -> List[str]:
     """Read the content of a file.
 
     You may use this to get the content of, for e.g., requirements.txt, VERSION, etc.
     """
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    return open(os.path.join(os.path.dirname(__file__), fname)).readlines()
+
+
+def read_requirements(fname: str) -> List[str]:
+    lines = [
+        flip_git(stripped_line)
+        for stripped_line in (line.strip() for line in read_lines(fname))
+        if stripped_line != ""
+    ]
+
+    return lines
+
+
+def flip_git(s: str) -> str:
+    """Flip pip's git source from ``requirements.txt`` format to `setuptools` format.
+
+    If `s` is not a git source, return as-is.
+
+    Args:
+        s (str): a line in ``requirements.txt``.
+
+    Returns:
+        str: if `s` is ``git+.://gh.com/a/b@c#egg=d``, then return ``d @ git+.://gh.com/a/b@c``.
+        Otherwise, return as-is.
+    """
+    if not s.startswith("git+"):
+        return s
+    git_url, egg = s.rsplit("#", 1)
+    _, egg_name = egg.split("=")
+    return f"{egg_name} @ {git_url}"
 
 
 # Declare minimal set for installation
-required_packages: List[str] = []
+required_packages: List[str] = read_requirements("requirements.txt")
+extras = {
+    "dev": read_requirements("requirements-dev.txt"),
+    "test": read_requirements("requirements-test.txt"),
+}
 
 setup(
     name=_pkg,
     packages=find_packages(where="src"),
     package_dir={"": "src"},
+    include_package_data=True,
     version=_version,
     description="{{cookiecutter.description}}",
     long_description=read("README.md"),
@@ -53,10 +87,9 @@ setup(
         "License :: OSI Approved :: {{cookiecutter.open_source_license}}",
         {% endif -%}
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
     ],
-    python_requires=">=3.6.0",
+    python_requires=">=3.8.0",
     install_requires=required_packages,
+    extras_require=extras,
 )
